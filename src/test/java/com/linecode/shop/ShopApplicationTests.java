629b66653ab8066f2a9e6b48dc24dfcf7a48cbf9
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-03-18 07:25:54 -0300

    Update ShopApplicationTests.java

--- src/test/java/com/linecode/shop/ShopApplicationTests.java
+++ src/test/java/com/linecode/shop/ShopApplicationTests.java
@@ -1,3 +1,8 @@
+/*
+* Author:  Igor Vinicius Freitas de Souza
+* GitHub: https://github.com/igor1043
+* E-mail: igorviniciusfreitasouza@gmail.com
+*/
 package com.linecode.shop;
 
 import org.junit.Test;
